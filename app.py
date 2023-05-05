5806be32b33a7a879c13a6b61521c25987568428
ShiwenNi <ShiwenNi@users.noreply.huggingface.co>
2023-05-05 03:00:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ description = '''<div align='left'>
 
 如果觉得很卡，可以点击右上角的Duplicate this Space，把ChatResponse复制到你自己的Space中！
 
-本项目的[Github](https://github.com/nishiwen1214/ChatResponse)，欢迎Star和Fork，也欢迎大佬赞助让本项目快速成长！💗（[获取Api Key](https://chatgpt.cn.obiscr.com/blog/posts/2023/How-to-get-api-key/)）
+本项目的[Github](https://github.com/nishiwen1214/ChatReviewer)，欢迎Star和Fork，也欢迎大佬赞助让本项目快速成长！💗（[获取Api Key](https://chatgpt.cn.obiscr.com/blog/posts/2023/How-to-get-api-key/)）
 </div>
 '''
 
